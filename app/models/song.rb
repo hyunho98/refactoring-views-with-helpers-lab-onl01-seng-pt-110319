124fc7336fed0d50ad64fbd2eca0cbb388f729e9
Alex Lee <hyunho98@gmail.com>
2020-05-11 09:15:41 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,10 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    self.name
+    artist.name
   end
 
   def artist_name=(name)
-    self.name = name
+    artist.name = name
   end
 end
